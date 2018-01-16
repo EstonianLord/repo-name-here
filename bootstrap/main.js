744d4b7d2afb045f52fbd49b7487fd891e4d57bb
estonianlord <iknowujerk@hotmail.com>
2018-01-16 16:55:01 +0200

    bootstrapi all

---
+++ bootstrap/main.js
@@ -0,0 +1,8 @@
+function my_function() {
+    var x = document.getElementById("sub_menu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
