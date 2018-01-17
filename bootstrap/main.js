50e55b2e20dadc09240facc037af3a8e35df166a
estonianlord <iknowujerk@hotmail.com>
2018-01-17 13:33:37 +0200

    ok

--- bootstrap/main.js
+++ bootstrap/main.js
@@ -7,9 +7,21 @@ function my_function() {
         sub_menu.style.display = "none";
     }
 }
+
+document.getElementById("add_one").addEventListener("click", add_one);
+document.getElementById("add_one").addEventListener("click", add_color);
+
 function add_one() {
     var n = document.getElementById("n");
     var number = n.innerHTML;
     number++;
     n.innerHTML = number;
+}
+function add_color() {
+    var n = document.getElementById("n");
+    n.style.backgroundColor = "orange";
+    n.style.width = "25px";
+    n.style.height = "25px";
+    n.style.borderRadius = "10px";
+
 }
